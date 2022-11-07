6db980a6e34d63fe49738fcde381ffcfc49998c3
Pope_Rigby <poperigby@mailbox.org>
2022-11-07 14:01:36 -0800

    Move BigInteger implementation to cpp

--- BigInteger.h
+++ BigInteger.h
@@ -7,15 +7,15 @@
 #include <string>
 using namespace std;
 
-class IllegalArgumentException: public::exception{
+class IllegalArgumentException : public ::exception {
   public:
-    char * what(){
+    char *what() {
         return "IllegalArgumentException";
     }
 };
-class NosuchElementException: public::exception{
+class NosuchElementException : public ::exception {
   public:
-    char * what(){
+    char *what() {
         return "NosuchElementException";
     }
 };
@@ -23,81 +23,19 @@ class NosuchElementException: public::exception{
 class BigInteger {
   protected:
     DoublyLinkedList<int> dll;
+
   public:
-    
-
-    BigInteger(char digits[]){
-        string s(digits);
-
-        // validate all digits before any allocation
-        for (int i = 0; i < s.size(); i++){
-            if (!isdigit(s.at(i))) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        // insert digits on the list
-        for (int i = 0; i < s.size(); i++){
-            int digit = s.at(i) - '0';
-            dll.insertFirst(digit);
-        }
-
-
-    }; 
-
-    
-    BigInteger operator-(const BigInteger &other) {
-        return *this;
-    }; // tempory
-    // yet to be tested 
-    bool operator==(BigInteger &other) {
-       if (dll.getLength() != other.dll.getLength()) {
-            return false;
-        }
-
-        dll.setIteratorFirst();
-        other.dll.setIteratorFirst();
-        while (!dll.isIteratorNULL()) {
-            if (dll.getData() != other.dll.getData()) {
-                return false;
-            }
-            dll.next();
-            other.dll.next();
-        }
-        return true;
-
-
-    }; // tempory
-
+    BigInteger(char digits[]);
+    BigInteger operator-(const BigInteger &other);
+    // yet to be tested
+    bool operator==(BigInteger &other);
     // copy assignment operator
-    BigInteger operator=(const BigInteger &other) {
-        return *this;
-    }; // tempory
-
-    BigInteger operator+(BigInteger &other) {
-        return *this;
-    }; // tempory
-
-    bool operator>=(BigInteger &other) {
-        return true;
-    }; // tempory;
-
-    bool operator<=(BigInteger &other) {
-        return true;
-    }; // tempory;
-
-    bool operator>(BigInteger &other) {
-        return true;
-    }; // tempory;
-
-    bool operator<(BigInteger &other) {
-        return true;
-    }; // tempory;
-
-    bool isNegative() {
-        return true;
-    }; // tempory
-
-
+    BigInteger operator=(const BigInteger &other);
+    BigInteger operator+(BigInteger &other);
+    bool operator>=(BigInteger &other);
+    bool operator<=(BigInteger &other);
+    bool operator>(BigInteger &other);
+    bool operator<(BigInteger &other);
+    bool isNegative();
 };
 #endif
